f1d6fb7348d39a340ab111863f139286cbaa8f5b
Matt Boyle <matt@shapeways.com>
2015-12-30 15:07:36 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+__author__ = 'matt'
+
+from setuptools import setup, find_packages
+
+setup(
+    name='coyote-framework',
+    version='0.1.0',
+    author='Shapeways',
+    author_email='api@shapeways.com',
+    install_requires=[
+        'BeautifulSoup',
+        'EasyProcess==0.1.6',
+        'MySQL-python==1.2.5',
+        'Pillow==2.3.0',
+        'PyVirtualDisplay==0.1.5',
+        'argparse==1.2.1',
+        'beautifulsoup4==4.3.2',
+        'bitstring==3.1.3',
+    ],
+    packages=find_packages(),
+    description='Python-based testing framework',
+)
