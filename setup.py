0bc6fb0fdac4c1423e646cc047b79c1a8a2e32ff
Matt Boyle <matt@shapeways.com>
2015-12-30 15:15:00 +0000

    Add webdriverwrapper

--- setup.py
+++ setup.py
@@ -8,16 +8,6 @@ setup(
     version='0.1.0',
     author='Shapeways',
     author_email='api@shapeways.com',
-    install_requires=[
-        'BeautifulSoup',
-        'EasyProcess==0.1.6',
-        'MySQL-python==1.2.5',
-        'Pillow==2.3.0',
-        'PyVirtualDisplay==0.1.3',
-        'argparse==1.2.1',
-        'beautifulsoup4==4.3.2',
-        'bitstring==3.1.3',
-    ],
     packages=find_packages(),
     description='Python-based testing framework',
 )
