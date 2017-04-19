6b5fd838c0b6af4c497133b606cec52e1385423b
Anthony Heckmann <anthony.heckmann@gmail.com>
2017-04-19 13:17:21 -0400

    update download url for package

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email='api@shapeways.com',
     packages=find_packages(),
     url = 'https://github.com/Shapeways/coyote_framework',
-    download_url = 'https://github.com/Shapeways/coyote_framework/tarball/0.1',
+    download_url = 'https://github.com/Shapeways/coyote_framework/archive/0.1.2.tar.gz',
     description='Python-based testing framework',
 )
