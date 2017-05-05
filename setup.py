8a4cd34d3d85617df60f8e82d0d27922c23bf1de
Tony Heckmann <tonyh@shapeways.com>
2017-05-05 17:10:12 -0400

    change package to find_package()

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 __author__ = 'matt'
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='coyote-framework',
     version='0.1.4',
     author='Shapeways',
     author_email='api@shapeways.com',
-    packages=['coyote_framework'],
+    packages=find_packages(),
     package_data={'coyote_framework': ['config/**/*.cfg']},
     url = 'https://github.com/Shapeways/coyote_framework',
     download_url = 'https://github.com/Shapeways/coyote_framework/archive/0.1.2.tar.gz',
