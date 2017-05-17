ad24ca0318da16201bad07a3283a31a8a7726fd4
Tony Heckmann <tonyh@shapeways.com>
2017-05-17 19:11:52 -0400

    change  to alternate style of subclassing

--- coyote_framework/config/database_config.py
+++ coyote_framework/config/database_config.py
@@ -2,8 +2,8 @@ from coyote_framework.config.abstract_config import ConfigBase
 
 
 class DatabaseConfig(ConfigBase):
-
+    section = 'database' #specify what default section to use
     def __init__(self):
-        super(DatabaseConfig, self).__init__('database')
+        super(DatabaseConfig, self).__init__(self.section) #not actually necessary
 
 
