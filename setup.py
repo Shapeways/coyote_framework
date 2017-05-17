1cf9bdf028f4cca1868680b14d7ba71438fc9ba1
Tony Heckmann <tonyh@shapeways.com>
2017-05-17 16:19:29 -0400

    add ability to pass options through to underlying request object

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='coyote-framework',
-    version='0.1.4',
+    version='0.1.5',
     author='Shapeways',
     author_email='api@shapeways.com',
     packages=find_packages(),
     package_data={'coyote_framework': ['config/**/*.cfg']},
     url = 'https://github.com/Shapeways/coyote_framework',
-    download_url = 'https://github.com/Shapeways/coyote_framework/archive/0.1.2.tar.gz',
+    download_url = 'https://github.com/Shapeways/coyote_framework/archive/0.1.5.tar.gz',
     description='Python-based testing framework'
 )
