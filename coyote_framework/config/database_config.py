a07bc3e501c0ed2aaa03e37ce68eb76a0b32adbb
Peter Zhen <peterz@shapeways.com>
2018-03-14 13:50:23 -0400

    add backwards compatibility

--- coyote_framework/config/database_config.py
+++ coyote_framework/config/database_config.py
@@ -3,6 +3,6 @@ from coyote_framework.config.abstract_config import ConfigBase
 
 class DatabaseConfig(ConfigBase):
 
-    def __init__(self):
+    def __init__(self, target_database=None):
         database = target_database if target_database else 'database'
         super(DatabaseConfig, self).__init__(database)
